ecfdb81ae6ef25ba94678fb90ac0f11371adc510
JesseNReynolds <jessenreynolds@gmail.com>
2021-02-23 15:14:36 -0700

    Decided to start with the core functionality of picking a random restaurant to go to first

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
-import thunk  from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension'
 
 
 import store from './store/store'

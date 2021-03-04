0f9736e706ef423442b74a58afaea6bd0a67ab99
JesseNReynolds <jessenreynolds@gmail.com>
2021-03-03 17:34:21 -0700

    controlled checkboxes that default to checked

--- src/components/RandomFoodFinder/FilterCategories.js
+++ src/components/RandomFoodFinder/FilterCategories.js
@@ -3,33 +3,58 @@ import { connect } from 'react-redux';
 
 class FilterCategories extends React.Component {
 
+    constructor() {
+        super()
+        this.state = {}
+    }
+
     typesOfRestaurants = () => {
-        const arrOfCategoriesArrs = this.props.results.map( result => {
+        const arrOfCategoryArrs = this.props.results.map( result => {
             return result.categories
         })
 
-        const flattenedCategoriesObjsArr = arrOfCategoriesArrs.flat()
+        const flattenedCategoriesObjsArr = arrOfCategoryArrs.flat()
         
         const arrOfCategories = flattenedCategoriesObjsArr.map( obj => {
             return obj.title
         })
 
         const arrOfUniqueCategories = [...new Set(arrOfCategories)]
-        
+
         return arrOfUniqueCategories
+
+    }
+
+    handleChange = (event) => {
+        const target = event.target;
+        const value = target.checked;
+        const name = target.name;
+        this.setState({
+          [name]: value
+        });
     }
 
     render() {
 
         return (
             <div>
-                
-                <ul>
-                    {this.typesOfRestaurants().map(category => {
-                    return <li>{category}</li>
-                    }
-                    )}
-                </ul>
+                <form>
+                    {this.typesOfRestaurants().map((category, key) => {
+                        return (
+                            <div key={key}>
+                                <label>
+                                    {category}
+                                </label>
+                                <input
+                                type='checkbox'
+                                name={category}
+                                checked={this.state[category] === undefined ? true
+                                : this.state[category]}
+                                onChange={this.handleChange}/>
+                            </div>
+                        )
+                    })}
+                </form>
             </div>
         )
     }

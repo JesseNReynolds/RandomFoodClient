9eae130cacaf696086eaecb74050329b051accab
JesseNReynolds <jessenreynolds@gmail.com>
2021-03-11 16:18:19 -0700

    Updated routing fix typo

--- src/components/App.js
+++ src/components/App.js
@@ -20,7 +20,7 @@ export default class App extends React.Component {
                 <div className='content'>
                     <Route exact path='/'>
                         <Welcome/>
-                    <Route />
+                    </Route>
                     <Route exact path='/random'>
                         <RandomContainer/>
                     </Route>
